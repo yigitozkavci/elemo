397266648307531b19149fdb5da4429bdef8619c
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-08-21 00:00:55 +0300

    Introduce PMRef

--- src/Data/PreservedMap.hs
+++ src/Data/PreservedMap.hs
@@ -10,24 +10,28 @@ import           Data.Maybe      (fromMaybe)
 
 type Map v = IM.IntMap (Maybe v)
 
+newtype PMRef = PMRef Int
+
 empty :: Map v
 empty = IM.empty
 
-(<|) :: v -> Map v -> Map v
-v <| m = IM.insert (IM.size m) (Just v) m
+(<|) :: v -> Map v -> (PMRef, Map v)
+v <| m = (PMRef r, IM.insert r (Just v) m)
+  where
+    r = IM.size m
 
-(|>) :: Map v -> v -> Map v
+(|>) :: Map v -> v -> (PMRef, Map v)
 m |> v = v <| m
 
-lookup :: Int -> Map v -> Maybe v
-lookup k m =
+lookup :: PMRef -> Map v -> Maybe v
+lookup (PMRef k) m =
   fromMaybe Nothing $ IM.lookup k m -- Whether it doesn't exist or deleted, we return nothing.
 
-(!!) :: Map v -> Int -> Maybe v
-m !! k = lookup k m
+(!!) :: Map v -> PMRef -> Maybe v
+m !! r = lookup r m
 
 delete :: Int -> Map v -> Map v
 delete = IM.update (const (Just Nothing))
 
 fromList :: [v] -> Map v
-fromList = foldl (|>) empty
+fromList = foldl (\m v -> snd (m |> v)) empty
