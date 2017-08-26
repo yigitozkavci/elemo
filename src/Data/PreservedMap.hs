b16eade7e308ae3b1e0a2ef96c4edf44a4655b44
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-08-26 19:20:59 +0300

    Implement mapM for PreservedMap

--- src/Data/PreservedMap.hs
+++ src/Data/PreservedMap.hs
@@ -2,10 +2,13 @@
 -- that values cannot be deleted, and only way to insert element is to
 -- append one. All indexing logic is abstracted away for the user.
 
+{-# LANGUAGE TupleSections #-}
+
 module Data.PreservedMap where
 
 --------------------------------------------------------------------------------
 import           Control.Arrow (first, second)
+import           Control.Applicative (liftA2, pure)
 import qualified Data.IntMap   as IM
 import           Data.Maybe    (catMaybes, fromJust, fromMaybe, isJust)
 import           Prelude       hiding (lookup)
@@ -19,19 +22,19 @@ newtype PMRef a = PMRef Int
 empty :: Map v
 empty = IM.empty
 
-(<|) :: v -> Map v -> (PMRef v, Map v)
-v <| m = (PMRef r, IM.insert r (Just v) m)
+(<|) :: Maybe v -> Map v -> (PMRef v, Map v)
+v <| m = (PMRef r, IM.insert r v m)
   where
     r = IM.size m
 
-(|>) :: Map v -> v -> (PMRef v, Map v)
+(|>) :: Map v -> Maybe v -> (PMRef v, Map v)
 m |> v = v <| m
 
 -- Appends that ignore refs
-(<<|) :: v -> Map v -> Map v
+(<<|) :: Maybe v -> Map v -> Map v
 v <<| m = snd (v <| m)
 
-(|>>) :: Map v -> v -> Map v
+(|>>) :: Map v -> Maybe v -> Map v
 m |>> v = snd (m |> v)
 
 lookup :: PMRef v -> Map v -> Maybe v
@@ -44,14 +47,22 @@ m !! r = lookup r m
 delete :: Int -> Map v -> Map v
 delete = IM.update (const (Just Nothing))
 
-fromList :: [v] -> Map v
+fromList :: [Maybe v] -> Map v
 fromList = foldl (\m v -> snd (m |> v)) empty
 
 elems :: Map v -> [v]
 elems = catMaybes . IM.elems
 
-assocs :: Map v -> [(PMRef v, v)]
-assocs = Prelude.map (second fromJust) . Prelude.filter (isJust . snd) . Prelude.map (first PMRef) . IM.assocs
+assocs :: Map v -> [(PMRef v, Maybe v)]
+assocs = Prelude.map (first PMRef) . IM.assocs
 
 map :: (Maybe v -> Maybe n) -> Map v -> Map n
 map = IM.map
+
+-- | Map a monadic function and gather the results.
+mapM :: Monad m => (Maybe v -> m (Maybe n)) -> Map v -> m (Map n)
+mapM f m' = (fromList . Prelude.map snd) <$> mapM' f (assocs m')
+
+mapM' :: Monad m => (Maybe v -> m (Maybe n)) -> [(PMRef a, Maybe v)] -> m [(PMRef a, Maybe n)]
+mapM' f [] = return []
+mapM' f ((ref, val):xs) = liftA2 (:) ((ref,) <$> f val) (mapM' f xs)
