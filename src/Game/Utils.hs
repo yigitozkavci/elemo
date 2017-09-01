73a6795bfe4cdefd96c59506c1cce3352da70202
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-09-01 12:19:55 +0300

    Turns out Num class has a  function already

--- src/Game/Utils.hs
+++ src/Game/Utils.hs
@@ -14,13 +14,6 @@ tupleSum (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple f (x, y) = (f x, f y)
 
--- | Don't know if Haskell already has this, too lazy to look for
-sig :: (Ord a, Num a) => a -> Int
-sig x
-  | x > 0 = 1
-  | x < 0 = -1
-  | otherwise = 0
-
 -- | Here is the algorithm:
 --
 -- From (3, 2) to (10, 15) we don't just want this particule to move with (1, 1) vector. Instead, we want to move with probabilities (7/20, 13/20). This way we'll achieve a natural movement flow.
@@ -33,7 +26,7 @@ moveTowards rand (x, y) (tX, tY) = tupleSum (x, y) movVec
     xP = fromIntegral xD / sum
     yP = fromIntegral yD / sum
     decide :: Float -> Int
-    decide t = if abs t * 100 > fromIntegral (rand `mod` 100) then sig t else 0
+    decide t = if abs t * 100 > fromIntegral (rand `mod` 100) then floor (signum t) else 0
     movVec = (decide xP, decide yP)
 
 distance :: (Int, Int) -> (Int, Int) -> Int
